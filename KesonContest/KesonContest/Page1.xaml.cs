3734c233f05659cebe680280ecbefe5628771611
Vang Truong <truongtanvang@gmail.com>
2020-09-18 13:05:35 +0700

    Add project files.

---
+++ KesonContest/KesonContest/Page1.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace KesonContest
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        private void bt_Skip_Clicked(object sender, EventArgs e)
+        {
+            var pageOne = new Page1();
+            NavigationPage.SetHasNavigationBar(pageOne, false);
+            NavigationPage mypage = new NavigationPage(pageOne);
+        }
+    }
+}
